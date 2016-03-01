f1e0f926920d2378f024ba012f95bbab3461c348
liucheng <209207878@qq.com>
2016-03-01 15:38:15 +0800

    哈哈

--- 武汉二部iOS基础框架/Modules(具体业务模块)/SubContent/Liuc(刘成)/LiuCController.m
+++ 武汉二部iOS基础框架/Modules(具体业务模块)/SubContent/Liuc(刘成)/LiuCController.m
@@ -7,13 +7,52 @@
 //
 
 #import "LiuCController.h"
+#import "MJRefresh.h"
+
+@interface LiuCController()<UITableViewDataSource,UITableViewDelegate>
+
+@property (nonatomic,strong) NSMutableArray *dataSource;
+
+@end
 
 @implementation LiuCController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSLog(@"给自己加点东西呢");
+}
+
+- (void)creatContentView
+{
+    UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    [self.view addSubview:tableView];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.dataSource.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 8.0;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return 0.0001;
 }
 
 @end
